f3b52dd0edb6b4b816603ed2071d59e38d185e0f
chefleo <simon.digiorgio@gmail.com>
2024-01-31 11:24:06 +0100

    Added claim NFT basic (without aa)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {
   ConnectWallet,
+  useAddress,
   useContract,
   useContractRead,
   useContractWrite,
@@ -10,121 +11,41 @@ import "./styles/Home.css";
 import { address, abi } from "./constant/addressBook.json";
 
 export default function Home() {
-  // const { contract } = useContract(address, abi);
+  const address = useAddress();
 
   const { contract } = useContract(
-    "0x8e7C31ba0bB7d37251A6c3b9F6a9A2fD6fA55f76"
+    "0xc6117A943756157D9a7a9f3386d780D9107E4B05"
   );
 
-  const { data: totalSupply, isLoading } = useContractRead(
-    contract,
-    "totalSupply"
-  );
-
-  // console.log("totalSupply", totalSupply.toString());
-
-  // const { data: Contacts } = useContractRead(contract, "getContacts");
-
-  // console.log("contacts", Contacts);
-  // console.log("abi", abi);
-
-  // const { mutate: addContact } = useContractWrite(contract, "addContact");
+  const claimNFT = async () => {
+    try {
+      const data = await contract?.erc1155.claim(0, 1);
+      console.info("contract call successs", data);
+    } catch (err) {
+      console.error("contract call failure", err);
+    }
+  };
 
   return (
-    <main className="main">
+    <main className="">
       <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!{" "}
-          </p>
-
+        <div className="flex items-center justify-between py-8">
+          {/* Saga */}
+          {/* Put image */}
+          <p>Image</p>
           <div className="connect">
             <ConnectWallet />
           </div>
-          {/* <Web3Button
-            contractAddress={"0x7325BD0BD2dcd2cDfC5f43A2763a43bcFdA202e3"}
-            action={() =>
-              addContact({
-                args: ["0x03305A2C9d2030841576F7480D7E2eE45599f7D1", "John"],
-              })
-            }
-          >
-            Test Add Contact
-          </Web3Button> */}
-        </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+          {address ? (
+            <Web3Button
+              contractAddress={"0xc6117A943756157D9a7a9f3386d780D9107E4B05"}
+              action={() => claimNFT()}
+            >
+              Claim NFT
+            </Web3Button>
+          ) : (
+            <p>Connect your wallet</p>
+          )}
         </div>
       </div>
     </main>
